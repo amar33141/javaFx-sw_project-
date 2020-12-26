9af4686443ee03293d71464ab4b1eda5713f809a
kareem261 <64379540+kareem261@users.noreply.github.com>
2020-12-26 23:21:59 +0200

    Update notificationTemplatesController.java

--- Users/compuneer/Documents/NetBeansProjects/SW_project/src/sw_project/notificationTemplatesController.java
+++ Users/compuneer/Documents/NetBeansProjects/SW_project/src/sw_project/notificationTemplatesController.java
@@ -15,6 +15,12 @@ public boolean creat(@RequestParam(name="id") int id,@RequestParam(name="content
    return obj.creat(id,sub,con,avl,o);
 }
 
+@RequestMapping(value = "/update",method = RequestMethod.PUT)
+    public boolean update(@RequestParam(name="id") int id,@RequestParam(name="content") String con,@RequestParam(name="langauge") String avl,@RequestParam(name="subject") String sub,notification_template o)
+    {
+        return obj.update(id,sub,con,avl,o);
+    }
+
 
 
 
