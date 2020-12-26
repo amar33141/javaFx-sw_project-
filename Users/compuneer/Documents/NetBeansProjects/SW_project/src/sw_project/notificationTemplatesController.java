1931050dbe91b41b736ea9e32130f8c9eb9edf23
amar33141 <75093147+amar33141@users.noreply.github.com>
2020-12-26 23:19:33 +0200

    notificationTemplateController class with create

---
+++ Users/compuneer/Documents/NetBeansProjects/SW_project/src/sw_project/notificationTemplatesController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+public class notificationTemplatesController{
+    CRUD_operation obj=new dataBase_creation();
+
+
+@RequestMapping(value = "/create",method = RequestMethod.POST)
+public boolean creat(@RequestParam(name="id") int id,@RequestParam(name="content") String con,@RequestParam(name="langauge") String avl,@RequestParam(name="subject") String sub,notification_template o)
+{
+   return obj.creat(id,sub,con,avl,o);
+}
+
+
+
+
+
+}
