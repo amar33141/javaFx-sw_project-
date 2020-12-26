a518741068dd3f96d68fd50934625ef4c02b50f6
rehab678 <52706037+rehab678@users.noreply.github.com>
2020-12-26 23:45:23 +0200

    Add Email class

--- Users/compuneer/Documents/NetBeansProjects/SW_project/src/sw_project/Email.java
+++ Users/compuneer/Documents/NetBeansProjects/SW_project/src/sw_project/Email.java
@@ -1,19 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sw_project;
+package com.example.demo;
 
-/**
- *
- * @author compuneer
- */
-public class Email implements Sending {
-      private User obj_u;
-       public void Send(String x,User obj_u)
+import java.sql.*;
+
+public class Email implements Sending{
+    private static Email object;
+    private Email()
     {
-        System.err.println(x);
+
+    }
+    public static Email getInstance()
+    {
+        if(object==null)
+            object=new Email();
+        return object;
+    }
+
+    public void Send(int id,String massage)
+    {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection myCon = DriverManager.getConnection("jdbc:mysql://localhost/notification_template", "root", "");
+
+            Statement mystat = myCon.createStatement();
+            PreparedStatement ps = myCon.prepareStatement("INSERT into Mail_queue (id,massage) VALUES(?,?)");
+            ps.setInt(1, id);
+            ps.setString(2,massage);
+            ps.execute();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
-
