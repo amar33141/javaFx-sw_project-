3e573875939684d66d31fe91c94f6a61d93c3fe5
amar33141 <75093147+amar33141@users.noreply.github.com>
2021-01-05 13:12:25 +0200

    add function dequeue in Email class

--- Users/compuneer/Documents/NetBeansProjects/SW_project/src/sw_project/Email.java
+++ Users/compuneer/Documents/NetBeansProjects/SW_project/src/sw_project/Email.java
@@ -32,4 +32,26 @@ public class Email implements Sending{
             throwables.printStackTrace();
         }
     }
+    
+    public void dequeue() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection myCon = DriverManager.getConnection("jdbc:mysql://localhost/notification_template", "root", "");
+
+            Statement mystat = myCon.createStatement();
+            System.out.println("your notifications Sending now by Email....");
+            ResultSet myr = mystat.executeQuery("select * from Mail_queue");
+            int x = 0;
+            while (myr.next()) {
+                x = myr.getInt("id");
+                System.out.println(myr.getString("massage") + " by email");
+
+            }
+            int myrsf = mystat.executeUpdate("Delete from Mail_queue");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
